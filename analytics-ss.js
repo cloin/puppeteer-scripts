5f1994802c483934a3fb4ea6f7afdce1314df567
cloin <c.a.mcnaughton@gmail.com>
2020-03-16 19:02:06 -0400

    adding new page

--- analytics-ss.js
+++ analytics-ss.js
@@ -13,12 +13,12 @@ const fs = require('fs');
   console.info("Browser successfully started");
   
   let page = await browser.newPage();
-  await page.setViewport({ width: 1280, height: 800 })
+  await page.setViewport({ width: 1400, height: 1000 })
   await page.goto('https://cloud.redhat.com', { waitUntil: 'networkidle2' });
 
   console.info("Login to portal");
 
-  const [response] = await Promise.all([
+  const [login] = await Promise.all([
     page.waitForNavigation({ waitUntil: 'networkidle2' }),
     page.click('#page > header > div.pf-c-page__header-tools > button'),
   ]);
@@ -28,16 +28,26 @@ const fs = require('fs');
   await page.focus("#password");
   await page.keyboard.type(""); // password
 
-  const [response1] = await Promise.all([
+  const [submitLogin] = await Promise.all([
     page.waitForNavigation({ waitUntil: 'networkidle2' }),
     page.click('#_eventId_submit'),
   ]);
 
+  console.info(". Goto analytics");
   await page.goto('https://cloud.redhat.com/ansible/automation-analytics/clusters', { waitUntil: 'networkidle2' });
+  console.info(".   Take screenshot");
+  await page.screenshot({ path: './screenshots/analytics.jpg', type: 'jpeg', fullPage: true });
 
-  console.info("Take screenshot");
+  console.info(". Goto hub");
+  await page.goto('https://cloud.redhat.com/ansible/automation-hub', { waitUntil: 'networkidle0' });
+  console.info(".   Take screenshot");
+  await page.screenshot({ path: './screenshots/hub.jpg', type: 'jpeg', fullPage: true });
 
-  await page.screenshot({ path: './image.jpg', type: 'jpeg', fullPage: true });
+  console.info(". Goto vulnerability");
+  await page.goto('https://cloud.redhat.com/rhel/vulnerability/cves?impact=7&page=1&sort=-public_date', { waitUntil: 'networkidle2' });
+  console.info(".   Take screenshot");
+  await page.screenshot({ path: './screenshots/vulnerability.jpg', type: 'jpeg', fullPage: true });
+
   console.info("Closing browser");
 	await page.close();
   await browser.close();
